ae146d87419f567913419a36417d5c88f042c70a
Felipe C. Araujo <felipe.s-04@hotmail.com>
2023-11-15 23:11:49 -0300

    aula 05

---
+++ Weather App/Color+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Color+Extensions.swift
+//  Weather App
+//
+//  Created by Felipe C. Araujo on 15/11/23.
+//
+
+import Foundation
---
+++ Weather App/HourlyForecastCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HourlyForecastCollectionViewCell.swift
+//  Weather App
+//
+//  Created by Felipe C. Araujo on 14/11/23.
+//
+
+import UIKit
+
+class HourlyForecastCollectionViewCell: UICollectionViewCell {
+    
+}
